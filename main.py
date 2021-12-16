4cef9ebfc81ea0df92dbbb1a1066c10997d88a2d
Kei-Eff <karl.alberto@hotmail.com>
2021-12-16 23:51:24 +1100

    Added user schema, started user deck schema

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-
+db = SQLAlchemy()
 ma = Marshmallow()
 
 def create_app():
